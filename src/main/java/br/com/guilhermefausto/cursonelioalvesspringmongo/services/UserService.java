4eee2069709c2385787308d5f4ffba76804ad8f0
Guilherme Fausto <guilhermesfausto@gmail.com>
2021-12-13 15:18:08 -0300

    Conectando ao MongoDB com Service e Repository

---
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package br.com.guilhermefausto.cursonelioalvesspringmongo.services;
+
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
