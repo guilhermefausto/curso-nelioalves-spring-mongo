c7322761d408d63631a5b1f2dcd778266052385d
Guilherme Fausto <guilhermesfausto@gmail.com>
2021-12-13 16:35:29 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/resources/UserResource.java
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import br.com.guilhermefausto.cursonelioalvesspringmongo.dto.UserDto;
 import br.com.guilhermefausto.cursonelioalvesspringmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDto> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDto(obj));
     }
 }
--- src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/services/UserService.java
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.guilhermefausto.cursonelioalvesspringmongo.services;
 
 import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
 import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.UserRepository;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,13 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> optional = repo.findById(id);
+        if(optional.isEmpty()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return optional.get();
     }
 }
