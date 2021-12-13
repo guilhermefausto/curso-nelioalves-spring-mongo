89e97bbbb9f3b8028ad4b47fad5b92577bbab0bf
Guilherme Fausto <guilhermesfausto@gmail.com>
2021-12-13 10:43:16 -0300

    Project Created

---
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/CursoNelioalvesSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermefausto.cursonelioalvesspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioalvesSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioalvesSpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilhermefausto/cursonelioalvesspringmongo/CursoNelioalvesSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermefausto.cursonelioalvesspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioalvesSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
