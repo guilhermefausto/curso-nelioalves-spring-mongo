a0fbefae1e8ad64b2fd33acbe374a54b6a541b97
Guilherme Fausto <guilhermesfausto@gmail.com>
2021-12-14 09:26:02 -0300

    Criando entity Post com User aninhado

--- src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/config/Instantiation.java
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package br.com.guilhermefausto.cursonelioalvesspringmongo.config;
 
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.Post;
 import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.PostRepository;
 import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,13 +18,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User maria = new User(null,"Maria Brown","maria@gmail.com");
         User alex = new User(null,"Alex Green","alex@gmail.com");
         User bob = new User(null,"Bob Grey","bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2021"),"Partiu Viagem","Vou viajar para São Paulo", maria);
+        Post post2 = new Post(null, sdf.parse("28/03/2021"),"Bom dia","Hoje eu acordei feliz", maria);
+
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        postRepository.saveAll(Arrays.asList(post1,post2));
+
     }
 }
--- src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/repository/PostRepository.java
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/repository/PostRepository.java
@@ -1,12 +1,9 @@
 package br.com.guilhermefausto.cursonelioalvesspringmongo.repository;
 
-import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-
+public interface PostRepository extends MongoRepository<Post, String> {
 }
