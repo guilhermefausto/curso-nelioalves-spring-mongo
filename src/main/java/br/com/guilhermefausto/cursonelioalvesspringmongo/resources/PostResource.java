9f2cdc403b641b0e23323c5a5af52e1383393d0c
Guilherme Fausto <guilhermesfausto@gmail.com>
2021-12-14 10:10:12 -0300

    Obtendo Post por id

---
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package br.com.guilhermefausto.cursonelioalvesspringmongo.resources;
+
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.Post;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.dto.UserDto;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.services.PostService;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/br/com/guilhermefausto/cursonelioalvesspringmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package br.com.guilhermefausto.cursonelioalvesspringmongo.services;
+
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.Post;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.domain.User;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.dto.UserDto;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.PostRepository;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.repository.UserRepository;
+import br.com.guilhermefausto.cursonelioalvesspringmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> optional = repo.findById(id);
+        if(optional.isEmpty()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return optional.get();
+    }
+}
